51df76b053465d4411bd4d8a2d7a7cb9b91a5034
Zach Cheng <zzcheng@emory.edu>
2024-04-07 23:33:15 -0400

    redesign dashboard

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,28 +1,33 @@
-document.getElementById('imageInput1').addEventListener('change', function() {
-    loadImage(this, 'image1', 'uploadBlock1');
-});
-  
-document.getElementById('imageInput2').addEventListener('change', function() {
-    loadImage(this, 'image2', 'uploadBlock2');
-});
-
-function loadImage(input, imgId, uploadBlockId) {
-    const img = document.getElementById(imgId);
-    const uploadBlock = document.getElementById(uploadBlockId);
-    const imageContainer = uploadBlock.querySelector('.image-container');
+function readURL(input, previewId) {
     if (input.files && input.files[0]) {
-        const reader = new FileReader();
+        var reader = new FileReader();
         
-        reader.onload = function(e) {
+        reader.onload = function (e) {
+            var img = document.createElement('img');
             img.src = e.target.result;
-            img.style.display = 'block';
-            imageContainer.style.display = 'block';
-            // Hide box
-            const uploadInstructions = uploadBlock.querySelector('p');
-            if (uploadInstructions) {
-                uploadInstructions.style.display = 'none';
-            }
+            img.style.display = 'block'; // Make sure the image is visible
+
+            var preview = document.getElementById(previewId);
+            preview.innerHTML = ''; // Clear any existing content
+            preview.appendChild(img); // Add the new image
         }
+        
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+document.getElementById('uploadUserButton').addEventListener('click', function() {
+    document.getElementById('userFileInput').click();
+});
+
+document.getElementById('userFileInput').addEventListener('change', function() {
+    readURL(this, 'userImagePreview');
+});
+
+document.getElementById('uploadReferenceButton').addEventListener('click', function() {
+    document.getElementById('referenceFileInput').click();
+});
+
+document.getElementById('referenceFileInput').addEventListener('change', function() {
+    readURL(this, 'referenceImagePreview');
+});
