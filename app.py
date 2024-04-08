c56642ab3e8a3b80bd87e1f13a5ea3f5cb5f98c3
Matthew McCall <mattbear888@gmail.com>
2024-04-07 22:23:15 -0400

    ft. Analyze page

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ def login():
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/analyze')
+def analyze():
+    return render_template('analyze.html')
