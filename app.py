3deb56b04f2374560eb139570753917099449f4b
Zach Cheng <zzcheng@emory.edu>
2024-04-07 21:50:14 -0400

    add page routs and styles

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
